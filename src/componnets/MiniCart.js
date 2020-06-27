658ab686101772d98c03166b3e8e36ce1c9e80a2
Yarin Ayash <yarinayyash@gmail.com>
2020-06-27 20:08:23 +0300

    add minicart count items

--- src/componnets/MiniCart.js
+++ src/componnets/MiniCart.js
@@ -1,35 +1,53 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { connect } from "react-redux";
 import { removeFromCart, removeProductItem, addToCart } from "../store/actions";
 import Message from "./Notifications/Notification";
 
 function MiniCart(props){
+  const [total, setTotal] = useState(0)
     useEffect(() => {
-        const total = props.cart.reduce(
-            (prev, curr) => prev + curr.qty * curr.price,
-            0
-          );
-          console.log(total);
+      const calcPrice = props.cart.reduce(
+          (prev, curr) => prev + curr.qty * curr.price,
+          0
+      );
+      setTotal(calcPrice)
     }, [props.cart]);
 
     return(
-        <div className="mini-cart">
+        <div className="minicart">
+          <div>Cart 
+            <span className="minicart-count">
+              {props.cart.length}
+            </span>
+          </div>
+          <div className="minicart-items">
             {
                 props.cart.length ? 
                     props.cart.map((product) => {
-                        return <div key={product.id}>
-                            <img width="75" height="75" src={product.images[0].src}  alt="" />
-                            <div>{product.name}</div>
-                            <div>{product.price}</div>
-                            <div>{product.qty}</div>
-                            <button onClick={() => props.addToCart(product)}>+</button>
-                            <button onClick={() => props.removeProductItem(product)}>-</button>
-                            <button onClick={() => props.removeFromCart(product)}>Remove Item</button>
+                        return <div className="item" key={product.id}>
+                            <div className="item-image">
+                              <img width="75" height="75" src={product.images[0].src}  alt="" />
+                            </div>
+                            <div className="item-info">
+                              <div className="item-meta">
+                                <div>{product.name}</div>
+                                <div>{product.price}</div>
+                                <div>{product.qty}</div>
+                              </div>
+                              <div className="item-actions">
+                                <button onClick={() => props.addToCart(product)}>+</button>
+                                <button onClick={() => props.removeProductItem(product)}>-</button>
+                                <button onClick={() => props.removeFromCart(product)}>Remove Item</button>
+                              </div>
+                            </div>
                         </div>
                     })
-                : <Message type='alert' msg='Empty Cart' />
+                : <Message type='alert' msg='Empty Mini-Cart' />
             }
+            <div>{total}</div>
+          </div>
         </div>
+
     )
 }
 
